4e7e861920149fd53207bf11fdd83037ddbbe834
berringy <berringyilmaz@gmail.com>
2025-02-05 23:28:16 +0300

    T99

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,36 +1,38 @@
 import java.lang.reflect.WildcardType;
 
 public class MyClass {
-    public String string="This is a string";
+    public String string = "This is a string";
 
     //V2
-    public String string2="Bu da başka bir string.";
+    public String string2 = "Bu da başka bir string.";
 
     //V3
-    public String string3="Bu da cok daha başka bir string.";
+    public String string3 = "Bu da cok daha başka bir string.";
 
     //techpro branch: T1
-    public String string4="Bu techpro branchinde oluşturuldu";
+    public String string4 = "Bu techpro branchinde oluşturuldu";
 
     //techpro branch: T2
-    public String string5="Bu da techpro branchinde oluşturuldu";
+    public String string5 = "Bu da techpro branchinde oluşturuldu";
 
     //V4
-    public String string6="Biraz daha farklı bir string";
+    public String string6 = "Biraz daha farklı bir string";
 
     //Techpro Branch:T3
-    public String string7="Bu biraz değişik bir string.";
+    public String string7 = "Bu biraz değişik bir string.";
 
     //Birazcık değişiklik olsun t4
-    public String string8="Birazcık değişiklik iyidir";
+    public String string8 = "Birazcık değişiklik iyidir";
 
     //Biraz daha değişiklik yapalım
-    public String string9="superiz";
+    public String string9 = "superiz";
 
     //Burada conflict olacak
-    public String string10="2000 yıl sonra";
+    public String string10 = "2000 yıl sonra";
 
 
     //conf22
 
-    public String string11="Bu bir string mi acaba?";
+    public String string11 = "Bu bir string mi acaba?";
+
+}
